75b0f67984b8e3db871cc7fef3aa52016757ec1b
LeeJiSeop <jjiisub@naver.com>
2023-12-03 17:10:36 +0900

    feat: 작품, 작가 검색 기능

--- gallery/urls.py
+++ gallery/urls.py
@@ -6,7 +6,9 @@ app_name = 'gallery'
 
 urlpatterns = [
     path('artist/list/', views.ArtistListView.as_view(), name='artist-list'),
+    path('artist/search/', views.ArtistSearchView.as_view(), name='artist-search'),
     path('artwork/list/', views.ArtworkListView.as_view(), name='artwork-list'),
+    path('artwork/search/', views.ArtworkSearchView.as_view(), name='artwork-search'),
     path('artwork/create/', views.ArtworkCreateView.as_view(), name='artwork-create'),
     path('exhibition/create/', views.ExhibitionCreateView.as_view(), name='exhibition-create'),
 ]
--- gallery/views.py
+++ gallery/views.py
@@ -4,7 +4,7 @@ from django.views.generic.list import ListView
 
 from .forms import ArtworkCreateForm, ExhibitionCreateForm
 from core.mixins import ArtistRequiredMixin
-from .models import Artist, Artwork, Exhibition
+from .models import Artist, Artwork
 
 
 class ArtistListView(ListView):
@@ -12,11 +12,49 @@ class ArtistListView(ListView):
     ordering = '-created_at'
     context_object_name = 'artists'
 
+class ArtistSearchView(ListView):
+    model = Artist
+    context_object_name = 'artists'
+    template_name = "gallery/artist_list.html"
+    
+    def get_queryset(self):
+        option = self.request.GET.get("search-option")
+        keyword = self.request.GET.get("search-keyword")
+        queryset = []
+        if not keyword:
+            return queryset
+        if option in ['name', 'email']:
+            queryset = Artist.objects.filter(**{f'{option}__icontains': keyword})
+        elif option in ['gender', 'birth_date', 'phone']:
+            queryset = Artist.objects.filter(**{option: keyword})
+        return queryset
+
 
 class ArtworkListView(ListView):
     model = Artwork
     ordering = '-created_at'
     context_object_name = 'artworks'
+
+
+class ArtworkSearchView(ListView):
+    model = Artwork
+    context_object_name = 'artworks'
+    template_name = "gallery/artwork_list.html"
+    
+    def get_queryset(self):
+        option = self.request.GET.get("search-option")
+        keyword = self.request.GET.get("search-keyword")
+        queryset = []
+        if option=='title':
+            queryset = Artwork.objects.filter(title__icontains=keyword)
+        elif option=='price' or option=='size':
+            comp = self.request.GET.get("search-option-compare")
+            option_compare = '__gte' if comp=="more" else '__lte'
+            try:
+                queryset = Artwork.objects.filter(**{f'{option}{option_compare}': keyword})
+            except:
+                pass
+        return queryset
 
 
 class ArtworkCreateView(ArtistRequiredMixin, View):
