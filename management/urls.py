7c10be0ab757104ab92522926b85a11c28ffeaa4
LeeJiSeop <jjiisub@naver.com>
2023-12-04 19:52:02 +0900

    style: management 앱 PEP8 적용

--- management/urls.py
+++ management/urls.py
@@ -7,7 +7,7 @@ app_name = 'management'
 urlpatterns = [
     path('apply/', views.ApplymentManageView.as_view(), name='apply'),
     path('apply/search/', views.ApplymentSearchView.as_view(), name='apply-search'),
-    path('apply/download/', views.ApplymentDownloadView.as_view(), name='apply-download'),
+    path('apply/download/', views.ApplymentDownloadView.as_view(),
+         name='apply-download'),
     path('statistics/', views.ArtistStatisticsView.as_view(), name='statistics'),
 ]
-
--- management/views.py
+++ management/views.py
@@ -32,9 +32,12 @@ class ApplymentManageView(ManagerOnlyMixin, View):
                 'errors': '승인과 반려를 동시에 선택할 수 없습니다.',
             }
             return render(request, "management/applyment.html", context)
-        Applyment.objects.filter(id__in=approve_list).update(is_approved=True, is_rejected=False)
-        Applyment.objects.filter(id__in=reject_list).update(is_rejected=True, is_approved=False)
-        User.objects.filter(applyment__pk__in=approve_list).update(is_artist=True)
+        Applyment.objects.filter(id__in=approve_list).update(
+            is_approved=True, is_rejected=False)
+        Applyment.objects.filter(id__in=reject_list).update(
+            is_rejected=True, is_approved=False)
+        User.objects.filter(
+            applyment__pk__in=approve_list).update(is_artist=True)
         for pk in approve_list:
             applyment = Applyment.objects.get(pk=pk)
             new_artist = Artist.objects.create(
@@ -61,7 +64,8 @@ class ApplymentSearchView(ManagerOnlyMixin, ListView):
         if not keyword:
             return queryset
         if option in ['name', 'email']:
-            queryset = Artist.objects.filter(**{f'{option}__icontains': keyword})
+            queryset = Artist.objects.filter(
+                **{f'{option}__icontains': keyword})
         elif option in ['gender', 'birth_date', 'phone']:
             queryset = Artist.objects.filter(**{option: keyword})
         return queryset
@@ -73,12 +77,14 @@ class ApplymentDownloadView(ManagerOnlyMixin, View):
         response = HttpResponse(content_type='text/csv')
         response['Content-Disposition'] = 'attachment; filename="applyments.csv"'
         writer = csv.writer(response)
-        writer.writerow(['이름', '성별', '생년월일','이메일','연락처','신청 일시','승인', '반려'])
+        writer.writerow(['이름', '성별', '생년월일', '이메일',
+                        '연락처', '신청 일시', '승인', '반려'])
         for applyment in applyments:
-            gender_value = '남성' if applyment.gender=='m' else '여성'
+            gender_value = '남성' if applyment.gender == 'm' else '여성'
             is_approved_value = 'v' if applyment.is_approved else ''
             is_rejected_value = 'v' if applyment.is_rejected else ''
-            writer.writerow([applyment.name, gender_value, applyment.birth_date, applyment.email, applyment.phone, applyment.created_at, is_approved_value, is_rejected_value])
+            writer.writerow([applyment.name, gender_value, applyment.birth_date, applyment.email,
+                            applyment.phone, applyment.created_at, is_approved_value, is_rejected_value])
         return response
 
 
@@ -91,7 +97,8 @@ class ArtistStatisticsView(ManagerOnlyMixin, ListView):
     def get_queryset(self):
         queryset = Artist.objects.annotate(
             artwork_count=Count('artworks'),
-            artwork_count_lte_size_100=Count('artworks', filter=Q(artworks__size__lte=100)),
+            artwork_count_lte_size_100=Count(
+                'artworks', filter=Q(artworks__size__lte=100)),
             artwork_avg_price=Avg('artworks__price')
         )
         return queryset
