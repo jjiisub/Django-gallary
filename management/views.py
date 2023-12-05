bc0294418a6d5c40ec2cbb152a1167852141f673
LeeJiSeop <jjiisub@naver.com>
2023-12-05 22:54:12 +0900

    comment: management 앱 docstring

--- management/views.py
+++ management/views.py
@@ -13,6 +13,21 @@ from gallery.models import Artist
 
 
 class ApplymentManageView(ManagerOnlyMixin, View):
+    '''
+    작가등록신청관리 View
+
+    Raises:
+        GET:
+            NOT is_authenticated:   로그인 페이지
+            NOT is_manager:         401_UNAUTHORIZED
+        POST:
+            form invalid:           Form Error 출력
+
+    Returns:
+        GET:    작가등록신청관리 페이지
+        POST:   작가등록신청 일괄처리 요청 후 작가등록신청관리 페이지
+    '''
+
     def get(self, request):
         applyments = Applyment.objects.all().order_by('-created_at')
         context = {
@@ -53,11 +68,38 @@ class ApplymentManageView(ManagerOnlyMixin, View):
 
 
 class ApplymentSearchView(ManagerOnlyMixin, ListView):
+    '''
+    작가등록신청 검색 View
+
+    Attrs:
+        model:                  Applyment 모델
+        context_object_name:    "applyments"
+        template_name:          작가등록신청관리 템플릿
+
+    Raises:
+        NOT is_authenticated:   로그인 페이지
+        NOT is_manager:         401_UNAUTHORIZED
+
+    Returns:
+        No Keyword:     비어있는 목록 출력
+        Option:
+            name, email:                keyword를 포함하는 검색 결과
+            gender, birth_date, phone:  keyword와 일치하는 검색 결과
+    '''
     model = Applyment
     context_object_name = 'applyments'
     template_name = "management/applyment.html"
 
     def get_queryset(self):
+        '''
+        작가등록신청 검색 결과 queryset method
+
+        Returns:
+            No Keyword:     비어있는 목록 출력
+            Option:
+                name, email:                keyword를 포함하는 검색 결과
+                gender, birth_date, phone:  keyword와 일치하는 검색 결과
+        '''
         option = self.request.GET.get("search-option")
         keyword = self.request.GET.get("search-keyword")
         queryset = []
@@ -73,6 +115,17 @@ class ApplymentSearchView(ManagerOnlyMixin, ListView):
 
 
 class ApplymentDownloadView(ManagerOnlyMixin, View):
+    '''
+    작가등록신청내역 CSV 다운로드 View
+
+    Raises:
+        NOT is_authenticated:   로그인 페이지
+        NOT is_manager:         401_UNAUTHORIZED
+
+    Returns:
+        작가등록신청내역 CSV 파일
+    '''
+
     def get(self, request):
         applyments = Applyment.objects.all().order_by('-created_at')
         response = HttpResponse(content_type='text/csv')
@@ -90,12 +143,32 @@ class ApplymentDownloadView(ManagerOnlyMixin, View):
 
 
 class ArtistStatisticsView(ManagerOnlyMixin, ListView):
+    '''
+    작가 통계 View
+
+    Attrs:
+        model:                  작가 모델
+        context_object_name:    "artists"
+        template_name:          작가 통계 템플릿
+
+    Raises:
+        NOT is_authenticated:   로그인 페이지
+        NOT is_manager:         401_UNAUTHORIZED
+
+    Returns:
+        작가 통계 페이지
+    '''
     model = Artist
-    ordering = '-created_at'
     context_object_name = 'artists'
     template_name = "management/statistics.html"
 
     def get_queryset(self):
+        '''
+        작가 통계 queryset method
+
+        Returns:
+            작가 별 작품수, 100호 이하 작품수, 작품 평균가격 결과
+        '''
         queryset = Artist.objects.annotate(
             artwork_count=Count('artworks'),
             artwork_count_lte_size_100=Count(
